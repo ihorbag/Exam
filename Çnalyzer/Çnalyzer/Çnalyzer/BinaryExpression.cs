dfa38552ee2343bb5d11f52c426eae51084fa008
Misha-Hablevych <87700180+Misha-Hablevych@users.noreply.github.com>
2022-04-18 12:26:58 +0300

    Add files via upload

---
+++ Çnalyzer/Çnalyzer/Çnalyzer/BinaryExpression.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace АnalyzerModule
+{
+    internal delegate double Operator(double x, double y);
+
+    internal class BinaryExpression
+    {
+        protected BinaryExpression()
+        {
+        }
+
+        public BinaryExpression(BinaryExpression left, BinaryExpression right,
+            Operator op)
+        {
+            Left = left; Right = right; Operator = op;
+        }
+
+        public virtual double Value
+        {
+            get
+            {
+                try
+                {
+                    return Operator(Left.Value, Right.Value);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Invalid input Parameter");
+                    return 0;
+                }
+
+            }
+            protected set { }
+        }
+
+        public BinaryExpression Left;
+        public BinaryExpression Right;
+        public Operator Operator;
+    }
+
+    internal class BinaryAtomic : BinaryExpression
+    {
+        protected BinaryAtomic()
+        {
+        }
+
+        public BinaryAtomic(double value)
+        {
+            Value = value;
+        }
+
+        public override double Value
+        {
+            get;
+            protected set;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
